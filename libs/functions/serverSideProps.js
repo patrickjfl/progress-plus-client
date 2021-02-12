f2b044e6387e1d3f8ac1cd5dc0173efb47f17a35
Ionut Predoi <ipredoi27@gmail.com>
2021-02-12 11:18:47 +0000

    githubAPI  implementation in work

--- libs/functions/serverSideProps.js
+++ libs/functions/serverSideProps.js
@@ -2,6 +2,50 @@ import nookies from 'nookies';
 import { verifyIdToken } from '../../firebaseUtils/firebaseAdmin';
 import { backendUrl } from '../globalVariables/urls';
 
+export default async function serverSideProps(
+	context,
+	customFetchRequest,
+	githubApiFetch
+) {
+	try {
+		const cookies = nookies.get(context);
+		console.log(cookies);
+		const { token } = cookies;
+		const sessionData = await verifyIdToken(cookies.token);
+		const { uid, picture } = sessionData;
+
+		const res = await fetch(`${backendUrl}${uid}`, {
+			headers: { authorization: `Bearer ${token}` },
+		});
+		const userData = await res.json();
+		// console.log({ userData });
+		const { name } = userData.data[0];
+		let data = '';
+		if (customFetchRequest) {
+			data = await customFetchRequest(backendUrl, uid, cookies.token);
+		}
+		let githubData = '';
+		if (githubApiFetch) {
+			githubData = await githubApiFetch(cookies.gitHubApiToken);
+			//console.log(githubData);
+		}
+
+		return {
+			props: { session: { name, uid, picture, data, token, githubData } },
+		};
+	} catch (err) {
+		console.log(err);
+		context.res.writeHead(302, { Location: '/' });
+		context.res.end();
+		return { props: {} };
+	}
+}
+
+// -------------------> OLD CODE - to be removed at some point <------------------------------------
+/* import nookies from 'nookies';
+import { verifyIdToken } from '../../firebaseUtils/firebaseAdmin';
+import { backendUrl } from '../globalVariables/urls';
+
 export default async function serverSideProps(context, customFetchRequest) {
   try {
     const cookies = nookies.get(context);
@@ -31,3 +75,4 @@ export default async function serverSideProps(context, customFetchRequest) {
     return { props: {} };
   }
 }
+ */
